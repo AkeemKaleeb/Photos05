22680f83b25359a3a79d83412082da860a304fc0
KailebCole <kaileb.j.cole@gmail.com>
2025-03-17 00:10:46 -0400

    Basic Login Screen

--- src/main/java/Photos.java
+++ src/main/java/Photos.java
@@ -1,4 +1,7 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -10,13 +13,20 @@ import javafx.stage.Stage;
  */
 public class Photos extends Application {
     @Override
-    public void start(Stage primaryStage) {
-        System.out.println("Launching Photos Application...");
-        primaryStage.setTitle("Photos Application");
+    public void start(Stage primaryStage) throws Exception {
+        System.out.println("FXML Path: " + getClass().getResource("/view/LoginView.fxml"));
+
+        // Load the FXML File
+        Parent root = FXMLLoader.load(getClass().getResource("/view/LoginView.fxml"));
+
+        // Set up the scene and stage
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setTitle("Photo Album Login");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-        Application.launch(args);
+        launch(args);
     }
 }
--- src/main/java/view/LoginController.java
+++ src/main/java/view/LoginController.java
@@ -1,5 +1,8 @@
 package view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 /**
  * Controls the login view of the photo album application.
  * The login view allows users to log in to the application and redirects to admin or user interface.
@@ -8,5 +11,13 @@ package view;
  * @owner Maxime Deperrois
  */
 public class LoginController {
-    
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private void handleSubmitButtonAction() {
+        String username = usernameField.getText();
+        System.out.println("Username: " + username);
+        // Add your login logic here
+    }
 }
